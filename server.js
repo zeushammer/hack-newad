0986614d3e5651494569bb836a11fffcd0fd9c9f
Simon Chung <chung.simon@gmail.com>
2015-10-11 03:15:26 -0400

    fix sockets for express 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var config = require('config');
 var http = require('http').Server(app);
+// var io = require('socket.io')(server);
 var io = require('socket.io')(http);
 var marklogic = require('marklogic');
 var _ = require('lodash');
@@ -11,6 +12,8 @@ var _ = require('lodash');
 var db = marklogic.createDatabaseClient(config.marklogic.connection);
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
   console.log('a user connected');
@@ -27,23 +30,60 @@ db.documents.read('/docs/ads.json').result(function (document) {
 });
 
 // socket timer
-var socketTimer = setInterval(function () {
-  if (!_.isEmpty(ads)) {
-    serveAd();
-  }
-}, config.socket.interval);
+// var socketTimer = setInterval(function () {
+//   if (!_.isEmpty(ads)) {
+//     serveRandomAd();
+//   }
+// }, config.socket.interval);
 
-// serve ad
-var serveAd = function () {
+// serves random ad
+var serveRandomAd = function () {
   var ad = _.sample(ads);
   console.log('emitting ad:', ad);
   io.emit('advert', ad);
 };
 
+// serves ad by ad id
+var serveAd = function (id) {
+  console.log('finding ad:', id);
+  var ad = _.find(ads, _.matchesProperty('id', id));
+  console.log('returning ad:', ad);
+  return ad;
+};
+
+// trigger management
+app.get('/connect', function (req, res, next) {
+  res.sendFile(__dirname + '/public/connect.html');
+});
+
+// format: /trigger?id=14
+app.all('/trigger', function (req, res, next) {
+  var id = req.query.id;
+  console.log('trigger ad:', id);
+  var ad = serveAd(id) || null;
+  if (ad) {
+    res.send({
+      status: 200
+    });
+  } else {
+    res.send({
+      status: 400
+    });
+  }
+});
+
+app.get('/', function (req, res, next) {
+  res.sendFile(__dirname = '/public/index.html');
+});
+
 app.get('*', function (req, res) {
-  res.sendfile('public/index.html');
+  res.sendFile(__dirname + '/public/error.html');
 });
 
 app.listen(config.app.port, function () {
-  console.log('Server running at: ' + config.app.port);
+  console.log('Server listening on: ' + config.app.port);
+});
+
+http.listen(config.socket.port, function () {
+  console.log('Socket listening on: ' + config.socket.port);
 });
